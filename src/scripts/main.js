0a44f99ca6ba128e3c4da6c156000a7be9b7c23b
Sviat Torba <torba.svyatoslav.fox@gmail.com>
2021-11-11 13:37:17 +0200

    mobile meu fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,3 +4,19 @@ import smoothscroll from 'smoothscroll-polyfill';
 
 window.__forceSmoothScrollPolyfill__ = true;
 smoothscroll.polyfill();
+
+document.querySelectorAll('.nav-item').forEach(function(indicator) {
+  indicator.addEventListener('click', function() {
+    if (!indicator.classList.contains('dropdown')) {
+      document.querySelector('.navbar-toggler').classList.add('collapsed');
+      document.querySelector('.navbar-collapse').classList.remove('show');
+    }
+  })
+});
+
+document.querySelectorAll('.dropdown-item').forEach(function(indicator) {
+  indicator.addEventListener('click', function() {
+    document.querySelector('.navbar-toggler').classList.add('collapsed');
+    document.querySelector('.navbar-collapse').classList.remove('show');
+  })
+});
